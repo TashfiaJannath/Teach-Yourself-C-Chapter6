d3bd6b65b8ee81e6201f41d4fb3a28a96378923d
Tashfia Jannath <76718979+TashfiaJannath@users.noreply.github.com>
2021-03-05 00:59:15 +0800

    Create exe4.c

---
+++ exercise/exe4.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+int main ()
+{
+    char str[80], *p;
+    int i;
+    printf("Enter a string:");
+    gets(str);
+    p=str;
+    while(*p && *p!=' ') p++;
+
+        printf(p);
+
+
+
+}
