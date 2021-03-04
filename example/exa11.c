641519ca0fe61709f01d7ffdd95c060a06a573ff
Tashfia Jannath <76718979+TashfiaJannath@users.noreply.github.com>
2021-03-05 00:53:51 +0800

    Create exa11.c

---
+++ example/exa11.c
@@ -0,0 +1,13 @@
+#include <Stdio.h>
+void mystrcpy(char *to, char *from)
+{
+    while(*from) *to++=*from++;
+    *to='\0';
+}
+int main ()
+{
+    char str[]="this is";
+    mystrcpy(str,"a test");
+    printf(str);
+    return 0;
+}
